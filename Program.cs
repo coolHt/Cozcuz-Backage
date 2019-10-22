a386c0047dc3f421f3ffdb9f216b572cd4fd06d3
handsomeboyHT <ht2631503150@gmail.com>
2019-10-22 18:18:09 +0800

    Set up project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Cozcuz_Backage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
