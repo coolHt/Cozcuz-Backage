5a400ac16513478832b74f7801686cf53ca08259
handsomeboyHT <ht2631503150@gmail.com>
2019-10-23 22:49:48 +0800

    更新注释

--- ht_project/Startup.cs
+++ ht_project/Startup.cs
@@ -17,24 +17,28 @@ namespace ht_project
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
-        {
+        public Startup(IConfiguration configuration)  //IConfiguration 读取配置
+    {
             Configuration = configuration;
         }
 
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-          services.AddDbContext<TodoContext>(opt =>
-                opt.UseInMemoryDatabase("TodoList"));
+        public void ConfigureServices(IServiceCollection services) //执行于Configure之前, 用于配置应用的服务 
+    {  //IServiceCollection 拥有add{Service}扩展方法 来新增服务
+          services.AddDbContext<TodoContext>(opt =>  //这里添加一个服务，这个服务是属于DbContext的，而且是TodoContext的实例
+                opt.UseInMemoryDatabase("TodoList"));  
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
-        {
+
+        //Configure 主要用于 应用响应HTTP请求的方式
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env) //用于创建应用的请求管理通道 ( 添加中间件 )  IHostingEnvironment 按环境配置服务
+    {   
+
+            //use方法将一个或多个中间件组件添加到请求管道, 中间件组件用于负责调用管道中的下一个组件
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -45,7 +49,7 @@ namespace ht_project
                 app.UseHsts();
             }
 
-            app.UseHttpsRedirection();
+            app.UseHttpsRedirection();  
             app.UseMvc();
         }
     }
